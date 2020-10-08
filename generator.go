7290c45d88b198f8009be7da978dd34caac2ef00
Ryan Hancock <ryanleehancock@gmail.com>
2020-10-08 16:48:57 +0100

    added generator and db

---
+++ generator.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// GenerateString generates a random string using a charset
+func GenerateString(length int, charset string) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
+}
--- storage.go
+++ storage.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/jmoiron/sqlx"
+import (
+	"log"
+
+	"github.com/jmoiron/sqlx"
+)
 
 const url_db = "./url.db"
 
@@ -14,24 +18,40 @@ type urlDB struct {
 	db *sqlx.DB
 }
 
-schema := `CREATE TABLE urls (
+var schema = `CREATE TABLE urls (
     key varchar(10) NOT NULL PRIMARY KEY,
 	url text NOT NULL);`
-	
+
 func openDB() *sqlx.DB {
-	return sqlx.MustConnect("sqlite3", url_db)
+	db := sqlx.MustConnect("sqlite3", url_db)
+	db.MustExec(schema)
+
+	return db
 }
 
 func NewDB() *urlDB {
-	return &urlDB {
-		db: openDB()
+	return &urlDB{
+		db: openDB(),
 	}
 }
 
 func (u urlDB) Set(key string, value string) error {
+	_, err := u.db.NamedExec(`INSERT INTO urls (key, url) VALUES (:key,:url)`,
+		map[string]interface{}{
+			"key":          key,
+			"url": value,
+		})
+
+	if err != nil {
+		log.Printf("Set error %v", err.Error())
+	}
+
 	return nil
 }
 
-func (u urlDB) Get(key string) {
-	return
+func (u urlDB) Get(key string) string {
+	var url string
+	_ = u.db.Get(&url, "SELECT url FROM urls WHERE key=$1", key)
+
+	return url
 }
